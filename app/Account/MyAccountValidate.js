976d9a9ba745572b5be8ae02c330ae8fd7ea0423
Саша Калаш <kalash@Sasha-Kalash.local>
2018-05-13 13:46:50 +0300

    +

--- app/Account/MyAccountValidate.js
+++ app/Account/MyAccountValidate.js
@@ -12,8 +12,9 @@ angular
       //   var value = modelValue || viewValue;
       //   return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/.test(value);
       // }
+      vm.showVm = () => console.log('vm', vm);
       vm.formSubmit = function() {
         vm.loginForm.$setPristine();
-    };
-    })
+      };
+    });
    
