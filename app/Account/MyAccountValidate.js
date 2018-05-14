e362b05a26cc84c151cabb1f6199b0762d2aca42
Саша Калаш <kalash@Sasha-Kalash.local>
2018-05-14 21:12:56 +0300

    corrected validating forms fileds

--- app/Account/MyAccountValidate.js
+++ app/Account/MyAccountValidate.js
@@ -1,20 +1,17 @@
 'use strict';
 
 angular
-    .module('myApp')
+  .module('myApp')
    
     .controller('MyAccountValidate', function() {
       var vm = this;
       const name = vm.name;
       const email = vm.email;
       const phone =  vm.phone;
-      // email.$validators.validPhonenumber = function(modelValue, viewValue) {
-      //   var value = modelValue || viewValue;
-      //   return /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/.test(value);
-      // }
-      vm.showVm = () => console.log('vm', vm);
+    
       vm.formSubmit = function() {
         vm.loginForm.$setPristine();
       };
     });
+    
    
